9a56d8fcf1cb83e608e0af81cd6f03eee11e7c42
tichambers99 <khanhkhanhars@gmail.com>
2021-05-19 09:46:37 +0700

    add sidebar ui

--- request-gate-front/src/components/request/CreateRequest.js
+++ request-gate-front/src/components/request/CreateRequest.js
@@ -3,11 +3,11 @@ import React from 'react';
 import '../common.css';
 
 import { Col, Button, Input } from 'reactstrap'
-export default function Sidebar(){
+export default function CreateRequest(){
   return(
     <Col className="box">
       <div>
-        <div className='box__heading'>Create request</div>
+        <div className=''>Create request</div>
         <Button>Create</Button>
       </div>
       <div>
@@ -20,27 +20,28 @@ export default function Sidebar(){
         <textarea>
           Add a description
         </textarea>
+        <div> 
+          <label for="status">Status</label>
+          <select id="status">
+            <option>open</option>
+          </select>
 
-        <label for="status">Status</label>
-        <select id="status">
-          <option>open</option>
-        </select>
+          <label for="category">Category</label>
+          <select id="category">
+            <option>Equipment</option>
+            <option>Marketing</option>
+            <option>Coding</option>
+            <option>Finance</option>
+          </select>
 
-        <label for="category">Category</label>
-        <select id="category">
-          <option>Equipment</option>
-          <option>Marketing</option>
-          <option>Coding</option>
-          <option>Finance</option>
-        </select>
-
-        <label for="assign">Assign</label>
-        <select id="assign">
-          <option>Jack</option>
-          <option>Tom</option>
-          <option>Jerry</option>
-          <option>Massino</option>
-        </select>
+          <label for="assign">Assign</label>
+          <select id="assign">
+            <option>Jack</option>
+            <option>Tom</option>
+            <option>Jerry</option>
+            <option>Massino</option>
+          </select>
+        </div>
       </div>
     </Col>
   )
