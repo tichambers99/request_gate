638f7d1fa0648f75cf5ec1fb823a54e85523a7b5
tichambers99 <khanhkhanhars@gmail.com>
2021-05-19 10:56:26 +0700

    restyling create-request

--- request-gate-front/src/components/request/CreateRequest.js
+++ request-gate-front/src/components/request/CreateRequest.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { Col, Button, Input } from 'reactstrap'
+import { Col} from 'reactstrap'
 
 import '../common.css';
-import './createRequest.css'
+import './request.css'
 
 export default function CreateRequest(){
   return(
@@ -12,42 +12,46 @@ export default function CreateRequest(){
         <button className='button button--green'>Create</button>
       </div>
       <div className="createForm__input">
-        <Input 
+        <input 
           placeholder='Title'
           type='text'
         />
       </div>
-      <div>
-        <textarea className="createForm__textarea">
-          Add a description
-        </textarea>
-      </div>
-      <div className="createForm__listSelect"> 
-        <div className='select'>
-          <label for="status">Status</label>
-          <select id="status">
-            <option>open</option>
-          </select>
+      <div className='box'>
+        <div>
+          <textarea 
+            className="createForm__textarea"
+            placeholder="Add a description"
+          >
+          </textarea>
         </div>
-        
-        <div className='select'>
-          <label for="category">Category</label>
-          <select id="category">
-            <option>Equipment</option>
-            <option>Marketing</option>
-            <option>Coding</option>
-            <option>Finance</option>
-          </select>
-        </div>
+        <div className="createForm__listSelect"> 
+          <div className='select'>
+            <label for="status">Status</label>
+            <select id="status">
+              <option>open</option>
+            </select>
+          </div>
+          
+          <div className='select'>
+            <label for="category">Category</label>
+            <select id="category">
+              <option>Equipment</option>
+              <option>Marketing</option>
+              <option>Coding</option>
+              <option>Finance</option>
+            </select>
+          </div>
 
-        <div className='select'>
-          <label for="assign">Assign</label>
-          <select id="assign">
-            <option>Jack</option>
-            <option>Tom</option>
-            <option>Jerry</option>
-            <option>Massino</option>
-          </select>
+          <div className='select'>
+            <label for="assign">Assign</label>
+            <select id="assign">
+              <option>Jack</option>
+              <option>Tom</option>
+              <option>Jerry</option>
+              <option>Massino</option>
+            </select>
+          </div>
         </div>
       </div>
     </Col>
