d0e0f3b2eb99e6002ebf5bef48e99f847a2743a8
hangntt1210 <nguyenthuyhang991012@gmail.com>
2021-05-30 18:58:59 +0700

    them-model

--- app/comment.php
+++ app/comment.php
@@ -7,4 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class comment extends Model
 {
     //
+    protected $table = "comment";
+
+    public function cbnv(){
+    	return $this->belongTo('App\cbnv','id_cbnv','id');
+    }
+
+    public function request(){
+    	return $this->belongTo('App\request','id_request','id');
+    }
 }
--- app/request.php
+++ app/request.php
@@ -7,4 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class request extends Model
 {
     //
+    protected $table = "request";
+
+    public function cbnv(){
+    	return $this->belongTo('App\cbnv','id_author','id');
+    }
+
+    public function comment(){
+    	return $this->hasMany('App\request','id_request','id');
+    }
 }
